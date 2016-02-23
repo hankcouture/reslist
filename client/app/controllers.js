959ec1d414560cf45bd408da021b718c617495fb
Hank Couture <hankcouture@gmail.com>
2016-02-23 16:30:35 -0600

    added bootstrap and simple form input/list display

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -2,5 +2,13 @@ var home = angular.module('reslist.home', [])
 
 home.controller('HomeController', ['$scope', '$http', '$state', function($scope, $http, $state) {
 
+	$scope.searchValue = '';
+	$scope.resList = []
+
+	$scope.addRestaurant = function(value) {
+		$scope.searchValue = '';
+		$scope.resList.push({ name: value })
+	}
+
 
 }])
